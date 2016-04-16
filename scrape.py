2d22d180e80646707cf0947d0b0b07391598777e
Rohit Kamal Saxena <rohit.saxena1@myntra.com>
2016-04-16 22:47:18 +0530

    Elle Crawler

--- scrape.py
+++ scrape.py
@@ -6,6 +6,44 @@ import tags
 import string
 import json
 
+def start_elle():
+    seedURL = "http://elle.in/fashion/street-style/"
+    s = requests.Session()
+    http_response = s.get(seedURL)
+    scrapyObject = HtmlResponse(url="HTML Body", body=http_response.content)
+    url_xpath = '//div[contains(@class,"first")]/div[DIV_NO]/a/@href'
+    data_urls = []
+    for i in range(1,17):
+        ammended_xpath = url_xpath.replace("DIV_NO",str(i))
+        a = scrapyObject.xpath(ammended_xpath)
+        u = a.extract()
+        if u:
+            data_urls.append("http://elle.in"+ u[0])
+    print "Extracting data from: " + str(data_urls)
+
+    data = []
+    f = open("CrawledOutputElle.txt", 'w')
+    all_tags = []
+    for url in data_urls:
+        httpres = s.get(url)
+        s_object = HtmlResponse(url="HTML Body", body=httpres.content)
+        a = s_object.xpath('//p[contains(@class,"tags")]/a/span/text()')
+        extracted_data = a.extract()
+        all_tags = all_tags + extracted_data
+        #f.write(str(extracted_data))
+        print extracted_data
+#        printable = string.printable
+#        for line in extracted_data:
+#            text = filter(lambda x: x in printable, line)
+#            f.write(text + "\n")
+    seen = set()
+    seen_add = seen.add
+    all_tags = [x for x in all_tags if not (x in seen or seen_add(x))]
+    f.write(str(all_tags))
+    f.close()
+
+
+
 def start_zara():
     seedURL_women = "http://www.zara.com/in/en/new-in/woman/view-all-c811528.html"
     seedURL_men = "http://www.zara.com/in/en/new-in/man/view-all-c809013.html"
@@ -68,4 +106,4 @@ def start_with_file():
 
 
 if __name__ == "__main__":
-    start_zara()
+    start_elle()
