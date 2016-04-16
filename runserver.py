9b89c69aff9c5b192b4e2177870dfe8a5cc4b5cd
Naman Parikh <naman.parikh@myntra.com>
2016-04-17 01:12:04 +0530

    Added response storing in file

--- runserver.py
+++ runserver.py
@@ -7,10 +7,41 @@ app = Flask(__name__)
 
 @app.route('/tags/all')
 def get_tags_all():
-    response = [scrape.start_with_file_vogue(), scrape.create_zara_response()]
+    demo = True
+    if demo:
+        with open("FinalResponse.txt", 'r') as f:
+            response = f.readline()
+        print response
+        return response
+    demo = True
+    response = [scrape.create_vogue_response(demo), scrape.create_zara_response(demo), scrape.create_elle_response(demo)]
+    response = myntra_selection_gap.get_selection_gap(response)
+    print response
+    if response:
+        with open("FinalResponse.txt", 'w') as f:
+            f.write(json.dumps(response))
+    return json.dumps(response)
+
+
+@app.route('/brands/all')
+def get_brands_all():
+    response = [scrape.create_elle_response()]
     response = myntra_selection_gap.get_selection_gap(response)
     return json.dumps(response)
 
+
+@app.route('/tags/vogue')
+def get_tags_vogue():
+    response = [scrape.start_with_file_vogue()]
+    response = myntra_selection_gap.get_selection_gap(response)
+    return json.dumps(response)
+
+
+@app.route('/tags/zara')
+def get_tags_zara():
+    response = [scrape.create_zara_response()]
+    response = myntra_selection_gap.get_selection_gap(response)
+    return json.dumps(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=12313, threaded=True, debug=True )
--- scrape.py
+++ scrape.py
@@ -8,7 +8,9 @@ import json
 import re
 
 
-def start_elle():
+def start_elle(demo=False):
+    if demo:
+        return
     seedURL = "http://elle.in/fashion/street-style/"
     s = requests.Session()
     http_response = s.get(seedURL)
@@ -41,12 +43,18 @@ def start_elle():
     seen = set()
     seen_add = seen.add
     all_tags = [x for x in all_tags if not (x in seen or seen_add(x))]
-    f.write(str(all_tags))
+    printable = string.printable
+    for tag in all_tags:
+        text = filter(lambda x: x in printable, tag)
+        f.write(str(text) + "\n")
+    # f.write(str(all_tags))
     f.close()
 
 
+def start_zara(demo=False):
+    if demo:
+        return
 
-def start_zara():
     seedURL_women = "http://www.zara.com/in/en/new-in/woman/view-all-c811528.html"
     seedURL_men = "http://www.zara.com/in/en/new-in/man/view-all-c809013.html"
     s = requests.Session()
@@ -65,10 +73,16 @@ def start_zara():
     top_zara_article_types_men = a.extract()
     top_zara_article_types_men = [str(x.lower()) for x in top_zara_article_types_men[0:50]]
     [response["tags"].append(x) for x in top_zara_article_types_men[0:50]]
+    f = open("CrawledOutputZara.txt", "w")
+    for r in response["tags"]:
+        f.write(r + "\n")
+    f.close()
     return response
 
 
-def start_vogue():
+def start_vogue(demo=False):
+    if demo:
+        return
     seedURL = "http://www.vogue.in/fashion/fashion-trends/"
     s = requests.Session()
     http_response = s.get(seedURL)
@@ -85,7 +99,7 @@ def start_vogue():
 
     print "Extracting data from: " + str(data_urls)
 
-    f = open("CrawledOutput.txt", 'w')
+    f = open("CrawledOutputVogue.txt", 'w')
     for url in data_urls:
         httpres = s.get(url)
         s_object = HtmlResponse(url="HTML Body", body=httpres.content)
@@ -101,8 +115,10 @@ def start_vogue():
 def start_with_file_vogue():
     f = open("CrawledOutput.txt", 'r')
     output_tags = {}
+    para = ""
     for line in f:
-        output_tags.update(tags.main(line))
+        para += line + "\n"
+    output_tags = tags.main(para)
     json_response = tags.create_response(output_tags)
     return add_source(json_response, "vogue")
 
@@ -113,11 +129,29 @@ def add_source(response, source):
     return response
 
 
-def create_zara_response():
-    #out = start_zara()
-    #values = out["tags"]
-    #print len(values)
-    values = ['culottes', 'oversized studio shirt', 'studs and chain cross body bag', 'long embroidered bomber jacket', 'multicoloured striped dress', 'frayed peplum top', 'leather platform slides', 'white dungarees with rips', 'blouse with lace trim', 'denim dress', 'high waist skinny trousers', 'denim shirt dress', 'bandana print silk style scarf', 'asymmetric hem shirt', 'cropped trousers with front pleat', 'roll-up sleeve jacket', 'tribal linen coat', 'short tribal skirt', 'crossover metallic sandals', 'bleach wash denim jacket', 'guipure lace bermuda shorts', 'guipure lace bomber jacket', 'wrap skirt', 'oversized linen t-shirt', 'sequin patchwork jacket', 'leather strap sandals', 'tie-dye midi skirt', 'blouse with open back', 'tie-dye jacquard culottes', 'tie-dye hand embroidered poncho', 'mid-rise biker trousers', 'short jumpsuit', 'lace midi dress', 'triple choker necklace', 'straight leg flowing trousers', 'jacket with asymmetric back', 'short jumpsuit', 'striped blouse', 'lace tube skirt', 'guipure lace top', 'bird print flowing bermuda shorts', 'casual roman sandals', 'short jumpsuit', 'mid-rise power stretch trousers', 'short sleeve sweater', 'flat metallic leather sandals', 'lace tube dress', 'openwork coat', 'guipure lace tube dress', 'leather sandals with buckle', 'bomber style jacket', 'plain twill shirt', 'striped back and pocket t-shirt', 'striped indigo shirt', 'striped back and pocket t-shirt', 'striped fabric seamed t-shirt', 'bull denim shirt', 'textured weave suit blazer', 'textured weave suit trousers', 'aviator sunglasses', 'faded denim jumpsuit', 't-shirt with zip on sleeves', 'sweatshirt', 'printed t-shirt', 'flower print t-shirt', 'bleach effect skinny trousers', 'bull denim shirt', 'vintage fade denim dungarees', 'faded indigo striped shirt', 'patch bomber jacket', 'raglan sleeve sweatshirt', 'raglan sleeve sweatshirt', 'textured t-shirt', 'creased texture t-shirt', 'darted trousers with cord', 'textured t-shirt', 'paisley print blazer', 'poplin shirt', 'stretch shirt with mandarin collar', 'paisley print bermuda shorts', 'striped shirt', 'horizontal stripe shirt', 'contrast blue jacket', 'tribal jacquard scarf', 'micro polka dot textured weave bermuda shorts', 'stretch shirt with mandarin collar', 'micro polka dot textured weave blazer', 'poplin shirt with contrasting collar', 'short sleeve nautical print polo shirt', 'short sleeve nautical print polo shirt', 'darted trousers with cord', 'darted trousers with cord', 'short sleeve t-shirt with oversized pocket', 'straw hat', 'short sleeve t-shirt with oversized pocket', 't-shirt with zip on sleeves', 'poplin shirt', 'short sleeve t-shirt with oversized pocket', 'short sleeve t-shirt with oversized pocket', 'street top']
+def create_elle_response(demo=False):
+    start_elle(demo)
+    f = open("CrawledOutputElle.txt", 'r')
+    tags = []
+    for line in f:
+        tags.append(line.rstrip()[:-1])
+    f.close()
+    res = {'source': 'elle', 'brands': tags}
+    return res
+
+
+def create_zara_response(demo=False):
+    out = start_zara(demo)
+    if demo:
+        f = open('CrawledOutputZara.txt', 'r')
+        values = []
+        for line in f:
+            values.append(line)
+        f.close()
+    else:
+        values = out["tags"]
+
+    # values = ['culottes', 'oversized studio shirt', 'studs and chain cross body bag', 'long embroidered bomber jacket', 'multicoloured striped dress', 'frayed peplum top', 'leather platform slides', 'white dungarees with rips', 'blouse with lace trim', 'denim dress', 'high waist skinny trousers', 'denim shirt dress', 'bandana print silk style scarf', 'asymmetric hem shirt', 'cropped trousers with front pleat', 'roll-up sleeve jacket', 'tribal linen coat', 'short tribal skirt', 'crossover metallic sandals', 'bleach wash denim jacket', 'guipure lace bermuda shorts', 'guipure lace bomber jacket', 'wrap skirt', 'oversized linen t-shirt', 'sequin patchwork jacket', 'leather strap sandals', 'tie-dye midi skirt', 'blouse with open back', 'tie-dye jacquard culottes', 'tie-dye hand embroidered poncho', 'mid-rise biker trousers', 'short jumpsuit', 'lace midi dress', 'triple choker necklace', 'straight leg flowing trousers', 'jacket with asymmetric back', 'short jumpsuit', 'striped blouse', 'lace tube skirt', 'guipure lace top', 'bird print flowing bermuda shorts', 'casual roman sandals', 'short jumpsuit', 'mid-rise power stretch trousers', 'short sleeve sweater', 'flat metallic leather sandals', 'lace tube dress', 'openwork coat', 'guipure lace tube dress', 'leather sandals with buckle', 'bomber style jacket', 'plain twill shirt', 'striped back and pocket t-shirt', 'striped indigo shirt', 'striped back and pocket t-shirt', 'striped fabric seamed t-shirt', 'bull denim shirt', 'textured weave suit blazer', 'textured weave suit trousers', 'aviator sunglasses', 'faded denim jumpsuit', 't-shirt with zip on sleeves', 'sweatshirt', 'printed t-shirt', 'flower print t-shirt', 'bleach effect skinny trousers', 'bull denim shirt', 'vintage fade denim dungarees', 'faded indigo striped shirt', 'patch bomber jacket', 'raglan sleeve sweatshirt', 'raglan sleeve sweatshirt', 'textured t-shirt', 'creased texture t-shirt', 'darted trousers with cord', 'textured t-shirt', 'paisley print blazer', 'poplin shirt', 'stretch shirt with mandarin collar', 'paisley print bermuda shorts', 'striped shirt', 'horizontal stripe shirt', 'contrast blue jacket', 'tribal jacquard scarf', 'micro polka dot textured weave bermuda shorts', 'stretch shirt with mandarin collar', 'micro polka dot textured weave blazer', 'poplin shirt with contrasting collar', 'short sleeve nautical print polo shirt', 'short sleeve nautical print polo shirt', 'darted trousers with cord', 'darted trousers with cord', 'short sleeve t-shirt with oversized pocket', 'straw hat', 'short sleeve t-shirt with oversized pocket', 't-shirt with zip on sleeves', 'poplin shirt', 'short sleeve t-shirt with oversized pocket', 'short sleeve t-shirt with oversized pocket', 'street top']
     print len(values)
     values = [re.sub('[^A-Za-z ]+', '', x) for x in values]
     values = [x.lower() for x in values]
@@ -126,7 +160,6 @@ def create_zara_response():
         'source': 'zara',
         'misc': []
     }
-    print len(values)
     for value in values:
         if value in mappings['direct_map']:
             print "Skipped value - " + str(value)
@@ -149,8 +182,19 @@ def create_zara_response():
     return res
 
 
+def create_vogue_response(demo=False):
+    start_vogue(demo)
+    f = open("CrawledOutputVogue.txt", 'r')
+    output_tags = {}
+    para = ""
+    for line in f:
+        para += line + "\n"
+    output_tags = tags.main(para)
+    json_response = tags.create_response(output_tags)
+    return add_source(json_response, "vogue")
+
 if __name__ == "__main__":
-    response = []
-    response.append(start_with_file_vogue())
-    response.append(create_zara_response())
+    response = [create_vogue_response(True), create_zara_response(True), create_elle_response(True)]
+    # response.append(create_zara_response())
     print json.dumps(response)
+    # create_elle_response()
