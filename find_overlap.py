1c47496036b36bc2db00779c51ff13e1383a60b9
Aman Khandelwal <aman.khandelwal@myntra.com>
2016-04-17 06:07:21 +0530

    extra changes

--- find_overlap.py
+++ find_overlap.py
@@ -1,5 +1,6 @@
 import tags
 import difflib
+import json
 
 vogue_data = {'foundation': ['Mousse Foundation'], 'chain': ['chain'], 'corset': ['closet'], 'tops': ['jersey top'], 'misc': ['everyday heavy duty makeup', 'minimum', 'gallery', 'place', 'heart-wrenching classic', 'blends', 'midway', 'high level', 'fashionista', 'heart-wrenching classic', 'skin types', 'skin types', 'faux fur bomber', 'everyday heavy duty makeup', 'everyday', 'off-shoulder blouses', 'style', 'acute aversion', 'gold accents', 'bell-bottoms', 'cabana', 'makes', 'faux fur bomber', 'long lasting', 'early 19th century', 'tones', 'Bollywood', 'erstwhile flower childs', 'bohemian sartorial sensibility', 'off-shoulder blouses', 'celebrity-inspired summer', 'cool evening winds', 'animal prints', 'perfect canvas', 'make-up free look', 'fashion accessory', 'no-make up', 'great coverage', 'collarbone steal', 'womens', 'thinking ahead', 'faux fur bomber', 'leather', 'light', 'popularity charts', 'Amazon India Fashion Week', 'larger-than-life persona', 'bohemian sartorial sensibility', 'pure style', 'animal prints', 'Rose Ivory', 'Rose Honey', 'round'], 'briefs': ['brief comebacks', 'brief comebacks'], 'source': 'vogue', 'trousers': ['trousers'], 'necklace': ['favourite statement necklace', 'favourite statement necklace'], 'earrings': ['stud earrings'], 'skirts': ['midi skirts', 'midi skirts', 'midi skirts']}
 zara_data = {'coats': ['tribal linen coat', 'openwork coat'], 'sweaters': ['short sleeve sweater'], 'sunglasses': ['aviator sunglasses'], 'shorts': ['paisley print bermuda shorts', 'micro polka dot textured weave bermuda shorts', 'guipure lace bermuda shorts', 'bird print flowing bermuda shorts'], 'sweatshirts': ['sweatshirt', 'raglan sleeve sweatshirt', 'raglan sleeve sweatshirt'], 'tops': ['street top', 'frayed peplum top', 'guipure lace top'], 'dungarees': ['vintage fade denim dungarees', 'white dungarees with rips'], 'misc': ['tribal jacquard scarf', 'culottes', 'leather platform slides', 'blouse with lace trim', 'bandana print silk style scarf', 'blouse with open back', 'tie-dye jacquard culottes', 'tie-dye hand embroidered poncho', 'striped blouse'], 'source': 'zara', 't-shirt': ['striped back and pocket t-shirt', 'striped back and pocket t-shirt', 'striped fabric seamed t-shirt', 't-shirt with zip on sleeves', 'printed t-shirt', 'flower print t-shirt', 'textured t-shirt', 'creased texture t-shirt', 'textured t-shirt', 'short sleeve t-shirt with oversized pocket', 'short sleeve t-shirt with oversized pocket', 't-shirt with zip on sleeves', 'short sleeve t-shirt with oversized pocket', 'short sleeve t-shirt with oversized pocket', 'oversized linen t-shirt'], 'bag': ['studs and chain cross body bag'], 'blazers': ['textured weave suit blazer', 'paisley print blazer', 'micro polka dot textured weave blazer'], 'skirts': ['short tribal skirt', 'wrap skirt', 'tie-dye midi skirt', 'lace tube skirt'], 'shirts': ['plain twill shirt', 'striped indigo shirt', 'bull denim shirt', 'bull denim shirt', 'faded indigo striped shirt', 'poplin shirt', 'stretch shirt with mandarin collar', 'striped shirt', 'horizontal stripe shirt', 'stretch shirt with mandarin collar', 'poplin shirt with contrasting collar', 'short sleeve nautical print polo shirt', 'short sleeve nautical print polo shirt', 'poplin shirt', 'oversized studio shirt', 'denim shirt dress', 'asymmetric hem shirt'], 'jackets': ['bomber style jacket', 'patch bomber jacket', 'contrast blue jacket', 'long embroidered bomber jacket', 'roll-up sleeve jacket', 'bleach wash denim jacket', 'guipure lace bomber jacket', 'sequin patchwork jacket', 'jacket with asymmetric back'], 'trousers': ['textured weave suit trousers', 'bleach effect skinny trousers', 'darted trousers with cord', 'darted trousers with cord', 'darted trousers with cord', 'high waist skinny trousers', 'cropped trousers with front pleat', 'mid-rise biker trousers', 'straight leg flowing trousers', 'mid-rise power stretch trousers'], 'necklace': ['triple choker necklace'], 'sandals': ['crossover metallic sandals', 'leather strap sandals', 'casual roman sandals', 'flat metallic leather sandals', 'leather sandals with buckle'], 'hat': ['straw hat'], 'dresses': ['multicoloured striped dress', 'denim dress', 'lace midi dress', 'lace tube dress', 'guipure lace tube dress'], 'jumpsuit': ['faded denim jumpsuit', 'short jumpsuit', 'short jumpsuit', 'short jumpsuit']}
@@ -52,6 +53,8 @@ def count_duplicates_within_source(json):
 def map_article_types(data, source):
     map = dict()
     map['misc'] = list()
+    source = data['source']
+    data.pop('source')
     tag_map = tags.create_set_article_types()
     for k, v in data.iteritems():
         for line in v:
@@ -97,8 +100,12 @@ def map_article_types(data, source):
 
 
 def merge_dicts(dict1, dict2):
-    cleaned_map1 = count_duplicates_within_source(dict1)
-    cleaned_map2 = count_duplicates_within_source(dict2)
+    map1 = map_article_types(dict1, 'vogue')
+    map2 = map_article_types(dict2, 'zara')
+    # print json.dumps(map1)
+    print json.dumps(map2)
+    cleaned_map1 = count_duplicates_within_source(map1)
+    cleaned_map2 = count_duplicates_within_source(map2)
     # print cleaned_map2
     # print cleaned_map1
     merged_map = cleaned_map1.copy()
