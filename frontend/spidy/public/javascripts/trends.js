5dbded75d245380e7467c39d17e05b7d7cd04eea
tirthankarsarkar <tirthankar.sarkar@myntra.com>
2016-04-17 06:57:47 +0530

    front end

---
+++ frontend/spidy/public/javascripts/trends.js
@@ -0,0 +1,87 @@
+Trend_APP = {
+   // 'selector_count':0,
+    'trends':{},
+    'render_selector' : function()
+    {
+        var article_types = [];
+        var data = Trend_APP.trends
+        for (var index in data){
+            article_types.push(index);
+
+        }
+        var options='';
+        for (index in article_types)
+            {
+                options = options + '<option value ="'+ article_types[index] + '">' + article_types[index] + '</option>'
+            }
+        var selector = '<select id="article_type_selector" class="form-control">' + options + '</select>'
+        var selector_div = '<div class="col-sm-4"></div><div class="col-sm-4"><center>Select Article Type</center><br><center><form role="form"><div class="form-group">' + selector + '</div></form></center></div>'
+        $('#selector').html(selector_div);
+    },
+    render_table:function(article_type)
+    {
+
+        $('#graph_div').html('<br><center>Loading Table ....</center>');
+
+        var obj;
+        for (category in Trend_APP.trends)
+        {
+            if( category == article_type )
+            {
+                obj = Trend_APP.trends[category];
+                break;
+            }
+        }
+        var thead = '<thead> <tr> <th> Fashion Trends </th><th>Google Score <th> Percentile </th> <th> ITF Score </th><th> Final Score </th> </tr></thead>';
+        var rows=''
+        for (trends in obj)
+        {
+            if (trends == undefined ){
+            continue;
+            }
+           // var percentage_gap = (obj[type]['gap_count']*100/obj[type]['total_count']);
+            rows = rows + '<tr><td>' + trends + '</td><td>' + obj[trends]['googlescore'].toFixed(2) + '</td><td>'+ obj[trends]['percentile'].toFixed(2) + '</td><td>' + obj[trends]['itf_score'].toFixed(2) + '</td><td>' + obj[trends]['final_score'].toFixed(2) + '</td></tr>'
+        }
+        var tbody='<tbody>' + rows + '</tbody>';
+        var table = '<table  id="trends_table" class="table table-bordered table-striped">' + thead + tbody + '</table>';
+        $('#graph_div').html('<br><center>' + table + '</center>');
+        $('#trends_table').DataTable(
+            {
+            "scrollY":        "100%",
+            "scrollCollapse": true,
+            "paging":         false,
+            "filter":false,
+            "order": [[ 4, "desc" ]],
+            "columnDefs": [
+                        { "width": "30%", "targets": 0 },
+                        { "bSortable": true, "aTargets":[1,2]}
+                        ]
+            });
+
+    }
+}
+$(document).ready(function()
+    {
+        console.log('Document Loaded');
+        $.ajax({
+                    url: '/getTrending',
+                    type: 'GET',
+                    success: function(response)
+                    {
+
+                        Trend_APP.trends = JSON.parse(response)
+                        Trend_APP.render_selector();
+                        $('#article_type_selector').trigger('change');
+                    },
+                    error:function(){
+                        console.log('Could not fetch selection gap data');
+                    }
+                })
+    });
+
+$(document).on('change', '#article_type_selector', function(event){
+    console.log('Returned');
+    console.log(event.target.value);
+    Trend_APP.render_table(event.target.value);
+
+});
--- frontend/spidy/routes/index.js
+++ frontend/spidy/routes/index.js
@@ -11,11 +11,22 @@ router.get('/selectionGap',
     function(req,res){
     res.render('selection_gap')
 });
+router.get('/trending',
+    function(req,res){
+    res.render('trending')
+});
 router.get('/getSelectionGap',function(req,res)
 {
     request("http://192.168.12.155:12313/tags/all", function(error, response, body) {
     //console.log(body);
     res.send(body);
+    });
 });
+router.get('/getTrending',function(req,res){
+    request('http://192.168.12.155:12313/tags/ranks',function(error,response,body){
+        console.log(body);
+        res.send(body);
+    })
 })
+
 module.exports = router;
